03d219c8ea627fc14ec83a9ce12c72c968b0c5db
shirokuma89dev <code@shirokuma89.dev>
2025-07-11 11:42:15 +0900

    Initial Commit

---
+++ ShiftEnterHelperTests/ShiftEnterHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftEnterHelperTests.swift
+//  ShiftEnterHelperTests
+//
+//  Created by 宮里 孝希 on 2025/07/11.
+//
+
+import Testing
+@testable import ShiftEnterHelper
+
+struct ShiftEnterHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
